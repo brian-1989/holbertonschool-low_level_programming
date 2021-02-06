078e6fe1e470941627aa060ee14f0682a0401cbd
Brian Zapata <3015@holbertonschool.com>
2021-02-06 00:14:34 +0000

    Patience, persistence and perspiration make an unbeatable combination for success Commit-2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,12 @@ int main(void)
 	for (i = 48 ; i < 58 ; i++)
 	{
 		putchar(i);
-		putchar(',');
+		if (i == 57)
+		{
+			break;
+		}
 		putchar(' ');
+		putchar(',');
 	}
 	putchar('\n');
 	return (0);
