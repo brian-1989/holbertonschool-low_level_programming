cc95d66fcf8fc0d6c6ebd76a6beddacc032b209e
Brian Zapata <3015@holbertonschool.com>
2021-02-10 01:08:18 +0000

    a + b Commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - Entry point
+ *
+ *@a: primero numero
+ *
+ *@b: segundo numero
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
