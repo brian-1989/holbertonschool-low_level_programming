862c7ba8fdb584e762c0795bbdc9b66bf7db77cc
Brian Zapata <3015@holbertonschool.com>
2021-02-11 18:50:29 +0000

    The numbers speak for themselves Commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
