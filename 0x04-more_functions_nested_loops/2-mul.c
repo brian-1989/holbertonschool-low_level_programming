0675127bbda5e3ed6527733ea656507902e6aae1
Brian Zapata <3015@holbertonschool.com>
2021-02-11 18:35:17 +0000

    Collaboration is multiplication Commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - Entry point
+ *
+ *@a: primero numero
+ *
+ *@b: segundo numero
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
