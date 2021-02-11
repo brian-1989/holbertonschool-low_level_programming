4e117062f1c2678dfb6a263f73318c75ff8bc078
Brian Zapata <3015@holbertonschool.com>
2021-02-11 18:04:40 +0000

    isupper Commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isupper - Entry point
+ *
+ * @c: mayúscula o minúscula
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
