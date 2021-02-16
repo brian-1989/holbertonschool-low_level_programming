a4b36dafe0f801afa4b17b874a98968486f8608c
Brian Zapata <3015@holbertonschool.com>
2021-02-16 16:54:14 +0000

    Don't swap horses in crossing a stream Commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * swap_int - Entry point
+ *
+ * @a: 1 entero
+ *
+ * @b: 2 entero
+ *
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
