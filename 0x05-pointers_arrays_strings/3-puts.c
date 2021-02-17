8f7bd452c2ae7342f0671cb344dfd75ccaa82b23
Brian Zapata <3015@holbertonschool.com>
2021-02-17 00:51:27 +0000

    I do not fear computers. I fear the lack of them Commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _puts - Entry point
+ *
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
