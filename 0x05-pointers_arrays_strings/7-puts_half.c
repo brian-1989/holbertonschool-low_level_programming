f4f00d173d9a2aaff105b85cffeff93093420175
Brian Zapata <3015@holbertonschool.com>
2021-02-17 05:06:00 +0000

    Winning is only half of it. Having fun is the other half Commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * puts_half - Entry point
+ *
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
+void puts_half(char *str)
+{
+	int i, j, k, contador, mitad_par, mitad_impar;
+
+	contador = 0;
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		contador++;
+	}
+	if (contador % 2 == 0)
+	{
+		mitad_par = contador / 2;
+		for (j = mitad_par ; str[j] != '\0' ; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		mitad_impar = ((contador - 1) / 2);
+		for (k = mitad_impar ; str[k] != '\0' ; k++)
+		{
+			_putchar(str[k]);
+		}
+	}
+	_putchar('\n');
+}
