ab3c5a06a5e01730989980a83160e600614fe3ce
Brian Zapata <3015@holbertonschool.com>
2021-02-17 22:39:34 +0000

    A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes Commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * rev_string - Entry point
+ *
+ * @s: string
+ *
+ * Return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+	int i, j, k, l, contador;
+
+	char cadena[98];
+
+	contador = 0;
+	l = 1;
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		contador++;
+	}
+	for (j = 0 ; j < contador ; j++)
+	{
+		cadena[contador - l] = s[j];
+		l++;
+	}
+	for (k = 0; k < contador ; k++)
+	{
+		s[k] = cadena[k];
+	}
+}
