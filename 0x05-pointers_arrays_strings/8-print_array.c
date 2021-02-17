7650397e99d6a3b56941d98c3e7794988dbd9011
Brian Zapata <3015@holbertonschool.com>
2021-02-17 05:54:03 +0000

    Arrays are not pointers Commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - Entry point
+ *
+ * @a: Array
+ *
+ * @n: numero de elementos
+ *
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", a[i]);
+			break;
+		}
+		printf("%d, ", a[i]);
+	}
+}
