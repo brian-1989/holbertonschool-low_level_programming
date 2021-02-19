5763f3c1630362886bd6e7b336c31810a3c7dae0
Brian Zapata <3015@holbertonschool.com>
2021-02-19 03:00:20 +0000

    strncpy Commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strncpy - Entry point
+ *
+ * @src: origen
+ *
+ * @dest: destino
+ *
+ * @n: bytes
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		*(dest + i) = src[i];
+	}
+	for ( ; i < n ; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
