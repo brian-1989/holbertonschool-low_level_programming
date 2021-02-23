a24450b48c3c499c9634d1959428c31b11f8ff2a
Brian Zapata <3015@holbertonschool.com>
2021-02-23 04:53:33 +0000

    strspn Commit-2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,9 +13,9 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j, contador;
 
 	contador = 0;
-	for (i = 0 ; accept[i] != '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' && s[i] != ' ' ; i++)
 	{
-		for (j = 0 ; s[j] != '\0' ; j++)
+		for (j = 0 ; accept[j] != '\0' ; j++)
 		{
 			if (s[i] == accept[j])
 			{
