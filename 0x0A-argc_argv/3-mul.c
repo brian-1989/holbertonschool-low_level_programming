770481630e9aad3bcda3afb826c2234fac9111da
Brian Zapata <3015@holbertonschool.com>
2021-02-27 18:14:18 +0000

    Point_3 Commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array of size argc
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int j, num_1, num_2, total;
+
+	j=1;
+	if (argc == 3)
+	{
+		num_1 = atoi(argv[j]);
+		j++;
+		num_2 = atoi(argv[j]);
+		total = num_1 * num_2;
+		printf("%d\n", total);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
