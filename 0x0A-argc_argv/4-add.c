4d3dd763490d99fbeb2d4c395c8acf8a8e005591
Brian Zapata <3015@holbertonschool.com>
2021-02-27 19:38:48 +0000

    Point_4 Commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array of size argc
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, num, suma;
+
+	suma = 0;
+	if (argc > 1)
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+			if (*argv[i] < '0' || *argv[i] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else if (atoi(argv[i]) < 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				num = atoi(argv[i]);
+				suma += num;
+			}
+		}
+		printf("%d\n", suma);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
