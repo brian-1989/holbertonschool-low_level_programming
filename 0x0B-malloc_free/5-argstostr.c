d96f3707b78dba04080c353d1acb30f0659eb29d
Brian Zapata <3015@holbertonschool.com>
2021-03-04 05:05:33 +0000

    Point_5 Commit

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * longitud - Entry point
+ *
+ * @s: string
+ *
+ * Return: Always 0 (Success)
+ */
+int longitud(char *s)
+{
+	int i, c = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		c++;
+	}
+	return (c);
+}
+/**
+ * argstostr - Entry point
+ *
+ * @ac: numero de elementos
+ * @av: array
+ * Return: Always 0 (Success)
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, h, p = 0, contador = 0;
+	char *cadena;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac ; i++)
+	{
+		contador = contador + longitud(av[i]);
+	}
+	cadena = malloc(sizeof(char) * (contador + ac));
+	if (cadena == NULL)
+		return (NULL);
+	for (j = 0 ; j < ac ; j++)
+	{
+		for (h = 0 ; h < longitud(av[j]); h++)
+		{
+			cadena[p] = av[j][h];
+			p++;
+		}
+		cadena[p] = '\n';
+		p++;
+	}
+	cadena[contador + ac] = '\0';
+	return (cadena);
+}
