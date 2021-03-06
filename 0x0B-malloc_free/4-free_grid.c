82995d976ac7aac7ae0e1ab66b291b92be5f3fe6
Brian Zapata <3015@holbertonschool.com>
2021-03-06 02:22:33 +0000

    Point_4 Commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - Entry point
+ *
+ * @grid: anchura del array
+ * @height: altura del array
+ * Return: Always 0 (Success)
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0 ; i < height ; i++)
+	{
+		free(grid[i]);
+	}
+}
