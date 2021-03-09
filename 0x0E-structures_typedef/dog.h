add5ea29cc34ae209161e7505b6dc29efd5e0521
Brian Zapata <3015@holbertonschool.com>
2021-03-09 03:51:33 +0000

    point_0 Commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Breve descripción
+* @name: Primer miembro
+* @age: Segundo miembro
+* @owner: Tercer miembro
+*Descripción: Descripción más larga
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
