76ee27f732f54139352a804c21f63a07264d262f
Brian Zapata <3015@holbertonschool.com>
2021-03-09 04:57:07 +0000

    Point_2 Commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - Entry point
+ * @d: puntero a al estructura dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	if (d->name == NULL)
+	{
+		printf("Nmae: (nil)");
+	}
+	if (d->age == '\0')
+	{
+		printf("Age: (nill)");
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)");
+	}
+	printf("Nmae: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
