c34a927e4b5477560aa204ffae87bf0e99b39d8c
Brian Zapata <3015@holbertonschool.com>
2021-03-12 23:30:23 +0000

    Point_2 Commit-2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", separator);
 		}
 		else
-			printf("(nill)%s", separator);
+			printf("(nill)");
 	}
 	printf("\n");
 }
