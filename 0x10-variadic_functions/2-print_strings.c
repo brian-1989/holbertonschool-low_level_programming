c4b4a28a311b9633df377f10283e2d002ff81a3b
Brian Zapata <3015@holbertonschool.com>
2021-03-12 23:26:27 +0000

    Point_2 Commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - Entry point
+ * @separator: Caracter para separar los argumentos
+ * @n: Numero de parámetros
+ * Return: suma
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *cadena;
+	va_list parametros;
+
+	va_start(parametros, n);
+	for (i = 0; i < n; i++)
+	{
+		cadena = va_arg(parametros, char *);
+		if (cadena != NULL)
+		{
+			printf("%s", cadena);
+			if (separator != NULL && i < n - 1)
+				printf("%s", separator);
+		}
+		else
+			printf("(nill)%s", separator);
+	}
+	printf("\n");
+}
