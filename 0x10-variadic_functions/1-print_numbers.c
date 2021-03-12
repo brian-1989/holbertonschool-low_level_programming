3eac8afa3fb02706be627e9a2da397ec93153c0d
Brian Zapata <3015@holbertonschool.com>
2021-03-12 04:12:30 +0000

    Point_1 Commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - Entry point
+ * @separator: Caracter para separar los argumentos
+ * @n: Numero de parámetros
+ * Return: suma
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, num;
+	va_list parametros;
+
+	va_start(parametros, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		num = va_arg(parametros, int);
+		if (i == n - 1)
+		{
+			printf("%d\n", num);
+			break;
+		}
+		printf("%d%s", num, separator);
+	}
+	va_end(parametros);
+}
