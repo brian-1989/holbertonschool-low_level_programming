e0effa784025267836e7cab62206e2491a1dde04
Brian Zapata <3015@holbertonschool.com>
2021-03-25 04:44:16 +0000

    Point_9 Commit-12

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,9 +14,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0, j;
 	listint_t *temp, *insertar, *recorrer;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
-	insertar = malloc(sizeof(listint_t) * 1);
+	insertar = malloc(sizeof(listint_t));
 	if (insertar == NULL)
 		return (NULL);
 	temp = *head;
