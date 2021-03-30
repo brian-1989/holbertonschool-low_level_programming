cb4940634d94109add81d49a8467da62636ea631
Brian Zapata <3015@holbertonschool.com>
2021-03-30 00:11:26 +0000

    Point_0 Commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * read_textfile - Write what's inside a file.
+ * @filename: file to print.
+ * @letters: number of letters in the file.
+ * Return: the number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int abrir, lectura, escritura;
+	char *buf[2000];
+
+	if (filename == NULL)
+		return (0);
+	abrir = open(filename, O_RDONLY);
+	if (abrir == -1)
+		return (0);
+	lectura = read(abrir, buf, letters);
+	if (lectura == -1)
+		return (0);
+	escritura = write(STDOUT_FILENO, buf, letters);
+	if (escritura == -1)
+		return (0);
+	close(abrir);
+	return (lectura);
+}
