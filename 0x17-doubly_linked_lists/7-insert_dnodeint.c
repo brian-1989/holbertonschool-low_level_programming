402059a8fd3e54dba1a0035714111759c2e2e021
Brian Zapata <dzapata397@gmail.com>
2021-05-02 21:06:02 +0000

    point_7 Comit-4

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,12 +12,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0, j = 0;
 	dlistint_t *temp, *new_node, *travel, *after;
 
+	temp = *h, travel = *h;
+	if (h == NULL || *h == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	if (h == NULL || *h == NULL)
-		return (NULL);
-	temp = *h, travel = *h;
+	if (*h == NULL && idx == 0)
+	{
+		*h = new_node;
+		return (new_node);
+	}
 	for ( ; travel != NULL; j++)
 		travel = travel->next;
 	if (idx <= j)
