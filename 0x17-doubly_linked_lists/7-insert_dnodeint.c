a1a68f4708d9ae610176b5e55c1df52c82c0a77f
Brian Zapata <dzapata397@gmail.com>
2021-05-03 16:27:40 +0000

    Point_7 Commit-8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,16 +35,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->next = *h, *h = new_node;
 			new_node->prev = NULL, new_node->n = n;
 			temp = new_node->next, temp->prev = new_node;
-			return (new_node);
 		}
 		if (i == idx - 1)
 		{
 			new_node->n = n, new_node->next = temp->next;
 			new_node->prev = temp, temp->next = new_node;
 			after = temp->next, after->prev = new_node;
-			return (new_node);
 		}
 		temp = temp->next, i++;
+		return (new_node);
 	}
 	return (NULL);
 }
