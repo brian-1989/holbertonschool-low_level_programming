95296b1034fd6874bb05791cd3786098f118f6d6
Brian Zapata <dzapata397@gmail.com>
2021-05-28 00:16:03 +0000

    Created hash_table_create Commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - this function that create a hash table.
+ *@size: size of hash table
+ * Return: a pointer to the hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i = 0;
+	hash_table_t *c = NULL;
+
+	c = malloc(sizeof(hash_table_t));
+	if (c == NULL)
+		return (NULL);
+	c->size = size;
+	c->array = malloc(sizeof(hash_node_t *) * size);
+	while(i < size)
+		c->array[i] = NULL, i++;
+	return (c);
+}
