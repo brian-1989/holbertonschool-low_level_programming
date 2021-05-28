89bc4114c1f9475f550a32951b2f8d3cdcdd8f70
Brian Zapata <dzapata397@gmail.com>
2021-05-28 17:57:26 +0000

    Free to the new node Commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -45,6 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			new_node = add_node(ht->array[i], key, value);
 			ht->array[i] = new_node;
+			free(new_node);
 			return (1);
 		}
 		i++;
