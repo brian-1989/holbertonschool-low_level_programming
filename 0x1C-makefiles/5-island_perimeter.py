60c4d63f60051f80475ad07f1078659ec3249829
Brian Zapata <dzapata397@gmail.com>
2021-07-07 23:51:21 +0000

    The conditions to analyze, if there are a Zero

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,11 +11,22 @@ def island_perimeter(grid):
     Return the perimeter of an island.
 
     """
-    count_of_1 = 1
+    perimeter_island = 0
     for i in range(0, len(grid)):
         if len(grid) > 101 and len(grid[i]) > 101:
             return
         for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
-                count_of_1 += 1
-    return (2 * count_of_1)
+                # condition to see if up is Zero
+                if i is 0 or grid[i - 1][j] is 0:
+                    perimeter_island += 1
+                # condition to see if at left is Zero
+                if j is 0 or grid[i][j - 1] is 0:
+                    perimeter_island += 1
+                # condition to see if down is Zero
+                if i is len(grid) or grid[i + 1][j] is 0:
+                    perimeter_island += 1
+                # condition to see if at right is Zero
+                if j is len(grid[0]) or grid[i][j + 1] is 0:
+                    perimeter_island += 1
+    return perimeter_island
